f3ece87983bf1aada756b28eefb9937aab3264eb
Ira Rosen <IRAR@il.ibm.com>
2016-01-07 10:08:38 +0200

    Added getDB API to CouchDBServer

--- CouchDBServer.swift
+++ CouchDBServer.swift
@@ -47,6 +47,29 @@ public class CouchDBServer {
         req.end()
     }
     
+    public func getDB (dbName: String, callback: (CouchDB?, NSError?) -> ()) {
+        let requestOptions = CouchDBUtils.prepareRequest(self, method: "GET", path: "/\(Http.escapeUrl(dbName))", hasBody: false)
+        let req = Http.request(requestOptions) { response in
+            var error: NSError?
+            var db: CouchDB?
+            if let response = response {
+                if response.statusCode == HttpStatusCode.OK {
+                    db = CouchDB(server: self, dbName: dbName)
+                }
+                else {
+                    self.createDB(dbName, callback: callback)
+                }
+            }
+            else {
+                error = CouchDBUtils.createError(CouchDB.ERROR_INTERNAL_ERROR, id: nil, rev: nil)
+            }
+            
+            callback(db, error)
+        }
+        
+        req.end()
+    }
+    
     public func deleteDB (db: CouchDB, callback: (NSError?) -> ()) {
         deleteDB(db.name, callback: callback)
     }
