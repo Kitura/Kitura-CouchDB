6b631e990ea9e45549dd10ca3127909755e6d75c
rolivieri <roliv@us.ibm.com>
2016-01-29 14:04:31 -0600

    Adding support for cloudant... work in progress.

--- Sources/CouchDB/CouchDBClient.swift
+++ Sources/CouchDB/CouchDBClient.swift
@@ -18,24 +18,6 @@ public class CouchDBClient {
     self.connProperties = connectionProperties
   }
 
-/*
-  //TODO
-  public func test(document: JSON) {
-    print("In test method of couchDB client!")
-    self.couchDB.create2(document, connProperties: connProperties, callback: { (id: String?, rev:String?, document: JSON?, error: NSError?) in
-      if (error != nil) {
-
-        print("CRAP!")
-        print(error!.code)
-        print(error!.domain)
-        print(error!.userInfo)
-      } else {
-        print("Hmm.... did it just work?")
-      }
-    })
-  }
-*/
-
   public func database(dbName: String) -> Database {
       return Database(connProperties: self.connProperties, dbName: dbName)
   }
