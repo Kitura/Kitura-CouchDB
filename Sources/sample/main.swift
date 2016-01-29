133c057d364c1d9eb9863696630de9c47dbed214
rolivieri <roliv@us.ibm.com>
2016-01-29 13:52:42 -0600

    Adding support for cloudant... work in progress.

--- Sources/sample/main.swift
+++ Sources/sample/main.swift
@@ -22,6 +22,33 @@ import SwiftyJSON
 
 print("Starting sample program...")
 
+// Connection properties for test Cloudant instance
+let connProperties = ConnectionProperties(userName: "fee33f3a-cdbc-4c9b-bf9a-f1541ee68c06-bluemix",
+  password: "2e2c5dc953727c763ff19b1ff399bd8b97ef5e3d7c249e55879eb849deafe374", secured: false)
+
+let connPropertiesStr = connProperties.toString()
+print("connPropertiesStr:\n\(connPropertiesStr)")
+
+// Create couchDBClient instance using conn properties
+let couchDBClient = CouchDBClient(connectionProperties: connProperties)
+print("Hostname is: \(couchDBClient.connProperties.hostName)")
+
+// Create database instance to perform any document operations
+let database = couchDBClient.database("phoenix_db")
+
+// Read sample document
+database.retrieve("93868ba2bbea73154974a72eb3ef7144", connProperties: connProperties, callback: { (document: JSON?, error: NSError?) in
+  if (error != nil) {
+    print("Oops something went wrong; could not read document.")
+    print(error!.code)
+    print(error!.domain)
+    print(error!.userInfo)
+  } else {
+    print("Here is the JSON document returned from Cloudant:\n\t\(document)")
+  }
+})
+
+// Write document
 // JSON string
 let jsonStr =
   "{" +
@@ -36,29 +63,17 @@ let jsonStr =
 let jsonData = jsonStr.dataUsingEncoding(NSUTF8StringEncoding)
 // Convert NSData to JSON object
 let json = JSON(data: jsonData!)
-
-// Connection properties for test Cloudant instance
-let connProperties = ConnectionProperties(userName: "fee33f3a-cdbc-4c9b-bf9a-f1541ee68c06-bluemix",
-  password: "2e2c5dc953727c763ff19b1ff399bd8b97ef5e3d7c249e55879eb849deafe374", secured: false)
-
-let connPropertiesStr = connProperties.toString()
-print("connPropertiesStr:\n\(connPropertiesStr)")
-
-// Create couchDBClient instance using conn properties
-let couchDBClient = CouchDBClient(connectionProperties: connProperties)
-print("Hostname is: \(couchDBClient.connProperties.hostName)")
-
-// Create database instance to perform any document operations
-let database = couchDBClient.database("phoenix_db")
-database.retrieve("93868ba2bbea73154974a72eb3ef7144", connProperties: connProperties, callback: { (document: JSON?, error: NSError?) in
+database.create(json, connProperties: connProperties, callback: { (id: String?, rev:String?, document: JSON?, error: NSError?) in
   if (error != nil) {
-    print("Oops something went wrong...")
+    print("Oops something went wrong; could not persist document.")
     print(error!.code)
     print(error!.domain)
     print(error!.userInfo)
   } else {
-    print("Here is the JSON document returned from Cloudant:\n\t\(document)")
+    print("Here is the JSON document that we just wrote to Cloudant:\n\t\(document)")
   }
 })
+
+//Need tests for CouchDB
 
 print("Sample program completed its execution.")
