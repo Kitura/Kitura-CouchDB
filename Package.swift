9ec1ced4d00ccd0b9982c0de3ed7354956685871
Robert F. Dickerson <rfdickerson@gmail.com>
2016-02-09 13:49:50 -0600

    Using prerelease version numbers

--- Package.swift
+++ Package.swift
@@ -40,14 +40,14 @@ let package = Package(
     // as dependencies. For now, due to what seems to be a defect in SPM,
     // we are specifying these other dependencies.
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
-      .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 1),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", majorVersion: 1),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", majorVersion: 1),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-Pcre2.git", majorVersion: 1),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-CurlHelpers.git", majorVersion: 1),
-      .Package(url: "https://github.com/IBM-Swift/Kitura-HttpParserHelper.git", majorVersion: 1),
+      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/Kitura-Pcre2.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/Kitura-CurlHelpers.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/Kitura-HttpParserHelper.git", majorVersion: 0),
       .Package(url: swiftyJsonUrl, majorVersion: swiftyJsonVersion)
     ]
 )
