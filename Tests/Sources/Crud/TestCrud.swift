2311acdcade3c921f9a9a8b1229e684270dc5e73
Daniel Firsht <dfirsht@us.ibm.com>
2016-02-11 15:32:59 -0600

    added crud testcase

---
+++ Tests/Sources/Crud/TestCrud.swift
@@ -0,0 +1,198 @@
+/**
+* Copyright IBM Corporation 2016
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+import XCTest
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin
+#endif
+
+import Foundation
+import SwiftyJSON
+import CouchDB
+
+class TestCrud : XCTestCase {
+    var allTests : [(String, () throws -> Void)] {
+        return [
+            ("CrudTest", crudTest),
+        ]
+    }
+
+    var database: Database?
+    
+    let documentId = "123456"
+    
+    var jsonDocument: JSON?
+    
+    func crudTest() {
+    
+        // Read in credentials file
+        let fstream = fopen("Tests/Sources/Crud/credentials.json", "r")
+        XCTAssertNotNil(fstream, "Error opening credentials.json, try running from root directory")
+        
+        var credentialsString = ""
+        while(true) {
+            let c = fgetc(fstream)
+            if c == -1 {
+                break
+            }
+            credentialsString += String(Character(UnicodeScalar(UInt32(c))))
+        }
+        
+        // Convert JSON string to NSData
+        let credentialsData = credentialsString.bridge().dataUsingEncoding(NSUTF8StringEncoding)
+        // Convert NSData to JSON object
+        let credentialsJson = JSON(data: credentialsData!)
+        
+        guard let hostName = credentialsJson["host"].string,
+        let userName = credentialsJson["username"].string,
+        let password = credentialsJson["password"].string else {
+            XCTFail("Error in credentials.json")
+            exit(1)
+        }
+        print(">> Successfully read in credentials")
+        
+        // Connection properties for testing Cloudant or CouchDB instance
+        let connProperties = ConnectionProperties(hostName: hostName,
+            port: 80, secured: false,
+            userName: userName,
+            password: password)
+        
+        // Create couchDBClient instance using conn properties
+        let couchDBClient = CouchDBClient(connectionProperties: connProperties)
+        print("Hostname is: \(couchDBClient.connProperties.hostName)")
+        
+        // Create database instance to perform any document operations
+        database = couchDBClient.database("kitura_db")
+        
+        // Start tests...
+        createDocument()
+    }
+    
+    func chainer(document: JSON?, next: (revisionNumber: String) -> Void) {
+        if let revisionNumber = document?["rev"].string {
+            print("revisionNumber is \(revisionNumber)")
+            next(revisionNumber: revisionNumber)
+        } else if let revisionNumber = document?["_rev"].string {
+            print("revisionNumber is \(revisionNumber)")
+            next(revisionNumber: revisionNumber)
+        } else {
+            XCTFail(">> Oops something went wrong... could not get revisionNumber!")
+        }
+    }
+    
+    //Delete document
+    func deleteDocument(revisionNumber: String) {
+        database!.delete(documentId, rev: revisionNumber, failOnNotFound: true, callback: { (error: NSError?) in
+            if (error != nil) {
+                XCTFail("Error in rereading document \(error!.code) \(error!.domain) \(error!.userInfo)")
+                
+            } else {
+                print(">> Successfully deleted the JSON document with ID \(self.documentId) from CouchDB.")
+            }
+        })
+    }
+    
+    //Reread document to confirm update
+    func confirmUpdate() {
+        database!.retrieve(documentId, callback: { (document: JSON?, error: NSError?) in
+            if error != nil {
+                XCTFail("Error in rereading document \(error!.code) \(error!.domain) \(error!.userInfo)")
+            } else {
+                guard let document = document as JSON!,
+                    let id = document["_id"].string,
+                    let value = document["value"].string else {
+                        XCTFail("Error: Keys not found when rereading document")
+                        exit(1)
+                }
+                XCTAssertEqual(self.documentId, id, "Wrong documentId read from updated document")
+                XCTAssertEqual("value2", value, "Wrong value read from updated document")
+                print(">> Successfully confirmed update in the JSON document")
+                self.chainer(document, next: self.deleteDocument)
+            }
+        })
+    }
+    
+    //Update document
+    func updateDocument(revisionNumber: String) {
+        //var json = JSON(data: jsonData!)
+        jsonDocument!["value"] = "value2"
+        database!.update(documentId, rev: revisionNumber, document: jsonDocument!, callback: { (rev: String?, document: JSON?, error: NSError?) in
+            if (error != nil) {
+                XCTFail("Error in updating document \(error!.code) \(error!.domain) \(error!.userInfo)")
+            } else {
+                guard let document = document as JSON!,
+                    let id = document["id"].string else {
+                        XCTFail("Error: Keys not found when reading updated document")
+                        exit(1)
+                }
+                XCTAssertEqual(self.documentId, id, "Wrong documentId read from updated document")
+                print(">> Successfully updated the JSON document")
+                self.confirmUpdate()
+            }
+        })
+    }
+    
+    //Read document
+    func readDocument() {
+        database!.retrieve(documentId, callback: { (document: JSON?, error: NSError?) in
+            if error != nil {
+               XCTFail("Error in reading document \(error!.code) \(error!.domain) \(error!.userInfo)")
+            } else {
+                guard let document = document as JSON!,
+                    let id = document["_id"].string,
+                    let value = document["value"].string else {
+                        XCTFail("Error: Keys not found when reading document")
+                        exit(1)
+                }
+                XCTAssertEqual(self.documentId, id, "Wrong documentId read from document")
+                XCTAssertEqual("value1", value, "Wrong value read from document")
+                print(">> Successfully read the following JSON document: ")
+                print(document)
+                self.chainer(document, next: self.updateDocument)
+            }
+        })
+    }
+    
+    //Create document closure
+    func createDocument() {
+        
+        // JSON document in string format
+        let jsonStr =
+        "{" +
+            "\"_id\": \"\(documentId)\"," +
+            "\"coordinates\": null," +
+            "\"truncated\": false," +
+            "\"created_at\": \"Tue Aug 28 21:16:23 +0000 2012\"," +
+            "\"favorited\": false," +
+            "\"value\": \"value1\"" +
+        "}"
+        
+        // Convert JSON string to NSData
+        let jsonData = jsonStr.bridge().dataUsingEncoding(NSUTF8StringEncoding)
+        // Convert NSData to JSON object
+        jsonDocument = JSON(data: jsonData!)
+        database!.create(jsonDocument!, callback: { (id: String?, rev: String?, document: JSON?, error: NSError?) in
+            if (error != nil) {
+                XCTFail("Error in creating document \(error!.code) \(error!.domain) \(error!.userInfo)")
+            } else {
+                print(">> Successfully created the JSON document")
+                self.readDocument()
+            }
+        })
+    }
+}
