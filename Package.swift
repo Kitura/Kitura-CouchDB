3723b7365efc377640f1977e9439f3240c9212ed
Daniel Firsht <dfirsht@us.ibm.com>
2016-02-23 16:42:20 -0600

    updated Package.swift to use v0.3.0 of sys

--- Package.swift
+++ Package.swift
@@ -49,7 +49,7 @@ let package = Package(
 package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,2,0)..<Version(0,3,0)))
 package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", versions: Version(0,2,0)..<Version(0,3,0)))
 package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0))
-package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", versions: Version(0,2,0)..<Version(0,3,0)))
+package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Kitura-sys.git", versions: Version(0,3,0)..<Version(0,4,0)))
 package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Kitura-net.git", versions: Version(0,2,0)..<Version(0,3,0)))
 package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,2,0)..<Version(0,3,0)))
 package.dependencies.append(.Package(url: "https://github.com/IBM-Swift/Kitura-Pcre2.git", versions: Version(0,2,0)..<Version(0,3,0)))
