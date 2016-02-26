3018e67f682f6e26bf9a6af850c34d7ba28c8aa0
rolivieri <roliv@us.ibm.com>
2016-02-26 16:51:21 -0600

    IBM-Swift/Kitura#114 Made some minor code changes/refactoring to the couch db test cases.

--- Tests/Sources/CouchDBTests/Utils.swift
+++ Tests/Sources/CouchDBTests/Utils.swift
@@ -1,3 +1,19 @@
+/**
+* Copyright IBM Corporation 2016
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
 import XCTest
 #if os(Linux)
     import Glibc
@@ -9,6 +25,7 @@ import Foundation
 import SwiftyJSON
 
 class Utils {
+
     struct Credentials {
         let host:String
         let username:String
@@ -23,29 +40,33 @@ class Utils {
     static func readCredentials() -> Credentials {
         // Read in credentials file
         let fstream = fopen("Tests/Sources/CouchDBTests/credentials.json", "r")
-        XCTAssertNotNil(fstream, "Error opening credentials.json, try running from root directory")
-        
+        XCTAssertNotNil(fstream, "Error opening credentials.json. Please try running from root directory.")
+
         var credentialsString = ""
         while(true) {
-            let c = fgetc(fstream)
-            if c == -1 {
+            let char = fgetc(fstream)
+            // EOF?
+            if char == -1 {
                 break
             }
-            credentialsString += String(Character(UnicodeScalar(UInt32(c))))
+            credentialsString += String(Character(UnicodeScalar(UInt32(char))))
         }
-        
+
         // Convert JSON string to NSData
         let credentialsData = credentialsString.bridge().dataUsingEncoding(NSUTF8StringEncoding)
         // Convert NSData to JSON object
         let credentialsJson = JSON(data: credentialsData!)
-        
-        guard let hostName = credentialsJson["host"].string,
-        let userName = credentialsJson["username"].string,
-        let password = credentialsJson["password"].string else {
-            XCTFail("Error in credentials.json")
+
+        guard
+          let hostName = credentialsJson["host"].string,
+          let userName = credentialsJson["username"].string,
+          let password = credentialsJson["password"].string
+        else {
+            XCTFail("Error in credentials.json.")
             exit(1)
         }
-        print(">> Successfully read in credentials")
+
+        print(">> Successfully read in credentials.")
         return Credentials(host: hostName, username: userName, password: password)
     }
 }
