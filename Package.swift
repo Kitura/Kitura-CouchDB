b891a120717b3515fcb04acd06071c10af4b8ac7
Daniel Firsht <dfirsht@us.ibm.com>
2016-05-04 13:42:38 -0500

    Updated to new version.

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 11),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 6)
     ]
 )
