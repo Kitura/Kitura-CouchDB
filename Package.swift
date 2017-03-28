28f22177cb1dd819f53cdaee0743357e7096cc4d
Navneet Gupta <navneet@us.ibm.com>
2017-03-28 18:16:32 -0500

    IBM-Swift/Kitura#1047 Swift 3.1 changes

--- Package.swift
+++ Package.swift
@@ -29,8 +29,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 6),
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15)
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 16)
     ]
 )
