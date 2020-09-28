bebfd04dee9dc4986bd548c4c570db4583771a00
Fouad Hatem <fouadhatem@icloud.com>
2020-09-28 15:06:44 +0300

    Update package references

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/IBM-Swift/LoggerAPI.git", from: "1.7.0"),
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.7.0"),
         kituraNetPackage,
     ],
     targets: [
